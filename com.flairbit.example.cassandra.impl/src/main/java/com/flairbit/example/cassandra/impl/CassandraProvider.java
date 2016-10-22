fd1982286cbf98f9957fd0a972b789973998f4d1
matteorulli <matteo.rulli@gmail.com>
2016-10-22 14:25:57 +0200

    Create keyspace in case it's missing

--- com.flairbit.example.cassandra.impl/src/main/java/com/flairbit/example/cassandra/impl/CassandraProvider.java
+++ com.flairbit.example.cassandra.impl/src/main/java/com/flairbit/example/cassandra/impl/CassandraProvider.java
@@ -10,6 +10,7 @@ import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.log.LogService;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.KeyspaceMetadata;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.mapping.Mapper;
 import com.datastax.driver.mapping.MappingManager;
@@ -19,17 +20,28 @@ import com.flairbit.example.cassandra.api.Cassandra;
 public class CassandraProvider implements Cassandra {
 
 	private final static String KEYSPACE = "eclipsecon";
+	private final static String CREATE_KS_CQL = "CREATE KEYSPACE IF NOT EXISTS " + KEYSPACE + 
+			" WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};";
+	private final static String CREATE_TABLE_CQL = "CREATE TABLE " + KEYSPACE +".kvs (key UUID PRIMARY KEY, value TEXT);";
 	
 	@Reference
 	private LogService log;
-	
+
 	private Session session;
 	private Mapper<Message> mapper;
-	
+
 	@Activate
 	public void activate() {
 		try {
-			session = Cluster.builder().addContactPoint("localhost").build().connect(KEYSPACE);
+			Cluster cluster = Cluster.builder().addContactPoint("localhost").build();
+			KeyspaceMetadata meta = cluster.getMetadata().getKeyspace(KEYSPACE);
+			if (meta == null) {
+				try (Session s = cluster.connect()){
+					s.execute(CREATE_KS_CQL);
+					s.execute(CREATE_TABLE_CQL);
+				}
+			}
+			session = cluster.connect(KEYSPACE);
 			MappingManager mappingManager = new MappingManager(session);
 			mapper = mappingManager.mapper(Message.class);
 		} catch (Throwable tr) {
@@ -39,12 +51,12 @@ public class CassandraProvider implements Cassandra {
 			}
 		}
 	}
-	
+
 	@Deactivate
 	public void deactivate() {
 		session.close();
 	}
-	
+
 	@Override
 	public UUID trace(String message) {
 		Message entity = new Message(message);
@@ -54,8 +66,8 @@ public class CassandraProvider implements Cassandra {
 
 	@Override
 	public String getMessage(UUID key) {
-		 return Optional.ofNullable(mapper.get(key)).
-				 map(Message::getMessage).orElse(null);
+		return Optional.ofNullable(mapper.get(key)).
+				map(Message::getMessage).orElse(null);
 	}
 
 }
